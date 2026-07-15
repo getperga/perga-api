1ca50da311cbae42285f9ca9f8531367cbdc9a25
Dmitry Tiunov <dbtiunov@gmail.com>
2026-07-15 13:50:39 +0200

    Use postgres for tests instead of sqlite

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -6,6 +6,11 @@ os.environ["SECRET_KEY"] = 'test-secret-key'
 os.environ["CORS_ORIGINS"] = json.dumps(['http://localhost:5173', 'http://localhost:3000'])
 os.environ["IS_SIGNUP_DISABLED"] = "False"
 
+os.environ.setdefault("POSTGRES_HOST", "localhost")
+os.environ.setdefault("POSTGRES_USER", "test_user")
+os.environ.setdefault("POSTGRES_PASSWORD", "test_pass")
+os.environ.setdefault("POSTGRES_DB", "test_db")
+
 
 pytest_plugins = [
     'app.tests.fixtures.auth',
